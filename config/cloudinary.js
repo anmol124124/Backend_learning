26ac52282247e0571aee61e05fae72c473c9e9cf
anmolmadaan124 <anmol.madaan@edublockpro.com>
2025-12-29 15:27:17 +0530

    changes i cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -7,9 +7,9 @@ import { v2 as cloudinary } from "cloudinary";
 // Cloudinary ko batate hain ki humare account ka naam kya hai,
 // api key kya hai, aur secret key kya hai.
 cloudinary.config({
-  cloud_name: "dzgn2pdle",   // Cloudinary dashboard se lo
-  api_key: "136369346866993",         // Safe access key
-  api_secret: "949SpEiOJFkllat4UQF_-fTI96g",   // Secret key for secure upload
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,   // Cloudinary dashboard se lo
+  api_key: process.env.CLOUDINARY_API_KEY,         // Safe access key
+  api_secret: process.env.CLOUDINARY_API_SECRET,   // Secret key for secure upload
 });
 
 export default cloudinary;
